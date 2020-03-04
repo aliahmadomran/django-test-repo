064f25b1e272795623a17bfb68531dadf4611a5c
Ali Omran <aliomran810@gmail.com>
2020-03-04 12:47:56 +0200

    update tutorial 2.1

--- firstdjpro/polls/models.py
+++ firstdjpro/polls/models.py
@@ -17,3 +17,7 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
     def __str__(self):
         return self.choice_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        
